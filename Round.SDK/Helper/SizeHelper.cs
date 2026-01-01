9d3395d941977cc3b8b9137ba858e26c7dde378a
Dime <liy_0924@qq.com>
2026-01-01 16:45:43 +0800

    feat: 添加 SizeHelper.cs

---
+++ Round.SDK/Helper/SizeHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace Round.SDK.Helper;
+
+public class SizeHelper
+{
+    public static string FormatBytes(double bytes)
+    {
+        string[] suffixes = { "B", "KB", "MB", "GB", "TB" };
+        int counter = 0;
+        double number = bytes;
+
+        while (number >= 1024 && counter < suffixes.Length - 1)
+        {
+            number /= 1024;
+            counter++;
+        }
+
+        return $"{number:F1} {suffixes[counter]}";
+    }
+}
