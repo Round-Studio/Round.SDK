358f77074a67409bbd3f599001c8e8d0e3315afe
Dime <liy_0924@qq.com>
2026-04-04 18:29:25 +0800

    feat: 新增注册类

---
+++ Round.SDK/Entry/BedrockBoot/SettingPageInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace Round.SDK.Entry.BedrockBoot;
+
+public class SettingPageInfo
+{
+    public string Header { get; set; } = "My Setting";
+    public string Description { get; set; } = "My description";
+    public object? Page { get; set; } = "";
+    public bool IsUseFontIcon { get; set; } = true;
+    public string IconSource { get; set; } = "\uE713";
+}
--- Round.SDK/Plugin/BedrockBoot/Register/RegisterService.cs
+++ Round.SDK/Plugin/BedrockBoot/Register/RegisterService.cs
@@ -1,4 +1,5 @@
-﻿using Round.SDK.Entry.BedrockBoot;
+﻿using System.Diagnostics;
+using Round.SDK.Entry.BedrockBoot;
 
 namespace Round.SDK.Plugin.BedrockBoot.Register;
 
@@ -16,8 +17,18 @@ public class RegisterService
     }
     public static void RegisterLaunchingEvent(Action<string> action)
     {
-        Console.WriteLine($@"注册启动操作 {action.Method.Name}");
+        Console.WriteLine($@"注册启动前操作 {action.Method.Name}");
         API.LaunchingEvent.Add(action);
+    }
+    public static void RegisterLaunchedEvent(Action<(string,Process)> action)
+    {
+        Console.WriteLine($@"注册启动前操作 {action.Method.Name}");
+        API.LaunchedEvent.Add(action);
+    }    
+    public static void RegisterSettingPage(SettingPageInfo info)
+    {
+        Console.WriteLine($@"注册插件设置项 {info.Header}");
+        API.SettingItems.Add(info);
     }
 
     public class API
@@ -25,5 +36,7 @@ public class RegisterService
         public static Action<TopBarItemInfo>? RegisterNavigationBarItem { get; set; }
         public static List<InstanceControlItemInfo> InstanceControlItems { get; set; } = new();
         public static List<Action<string>> LaunchingEvent { get; set; } = new();
+        public static List<Action<(string, Process)>> LaunchedEvent { get; set; } = new();
+        public static List<SettingPageInfo> SettingItems { get; set; } = new();
     }
 }
